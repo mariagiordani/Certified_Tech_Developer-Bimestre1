20ad289cf43a0d640607c8812c68c7f074b98bfb
Marcos Paulo da Costa <marcos_nokbuk@hotmail.com>
2021-10-25 20:41:48 -0300

    Atividade Aula 5

---
+++ Treino/Treino1/tiposDeDados.js
@@ -0,0 +1,14 @@
+// Objeto Literal
+let moto = {
+    marca: "Honda",
+    ano: "2019",
+    modelo: "CB500"
+}
+
+console.log(moto)
+
+//Array
+
+let series = ["GOT", "BB", "GITH",NaN, "A"]
+
+console.log(series)
---
+++ Treino/Treino1/variaveis.js
@@ -0,0 +1,10 @@
+var nome = "Marcolino"
+console.log(nome)
+nome = "Marcoleiton"
+console.log(nome)
+
+if(true){
+     nome = 'Renato'
+    console.log(nome)
+}
+
