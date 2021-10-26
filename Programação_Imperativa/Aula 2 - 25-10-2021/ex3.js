97cab762338516c99cf0c854025f07178be95fbd
Marcos Paulo da Costa <marcos_nokbuk@hotmail.com>
2021-10-25 22:35:49 -0300

    Programação Imperativa Aula 5

---
+++ Programação_Imperativa/Aula 2 - 25-10-2021/ex3.js
@@ -0,0 +1,31 @@
+//Item 1
+var num1 = 2 + 3
+console.log(num1)
+
+//Item 2
+var num2 = 2 * 3
+console.log(num2)
+
+//Item 3
+var a = 2
+var b = 3
+var soma = a + b
+console.log(soma)
+
+//Item 4
+var num3 = num1 + num2
+console.log(num3)
+
+//Item 5
+soma = "1" + 1
+console.log(soma)
+
+//Item 6
+soma = "olá" + 23
+console.log(soma)
+
+//Item 7
+soma = "Mensagem Concatenada " + 99
+console.log(soma)
+soma = 2020 + " 99"
+console.log(soma)
