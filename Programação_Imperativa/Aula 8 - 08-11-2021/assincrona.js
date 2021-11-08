8a3ac259dd97f51b4bdee9c29c2e76eef1933d59
Marcos Paulo da Costa <marcos_nokbuk@hotmail.com>
2021-11-08 20:50:00 -0300

    Estudo - Posicionamento

---
+++ Programação_Imperativa/Aula 8 - 08-11-2021/assincrona.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+function podeSubir(altura, acompanhante){
+ if (altura <= 200 && altura >= 140){
+return console.log("Acesso Autorizado")
+}else if(altura < 140 && altura >= 120){
+    if(acompanhante == "sim"){
+        return console.log("Acesso Autorizado com Acompanhante")
+
+    }else{
+        return console.log("Acesso Negado, necessário acompanhante")
+    }
+
+}else{
+         return console.log("Acesso Negado")
+}
+}
+
+podeSubir(125, "sim")
